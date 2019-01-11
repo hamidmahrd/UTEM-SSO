e4dcac85f498226fde215bd7a3f8c9d9b67acb2a
hamid <hamid.massoudi.my@gmail.com>
2019-01-11 12:17:09 +0800

    Sso_reports controller initialize.

---
+++ application/controllers/Sso_reports.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by Astiostech.
+ * User: hamid
+ * Date: 1/3/2018
+ * Time: 01:39 PM
+ */
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+require_once APPPATH . 'third_party/Cli.php';
+
+use Stunt\Console\Cli as Cli;
+
+class Sso_reports extends CI_Controller {
+    public $cli;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        set_time_limit(0);
+        $this->cli = new Cli('', '', array());
+
+    }
+
+    public function index()
+    {
+        if(is_cli())
+        {
+            $this->cli->out("[green_bold]SSO program help :.(".date('Y-m-d H:i:s').").[reset]\n")->new_line();
+            
+        }
+    }
+
+
+}
