2b67310b2adbd9c49d7cc0b979d68f4e13e824e6
hamid <hamid.massoudi.my@gmail.com>
2019-01-11 16:54:16 +0800

    Sso Reports, AD_List_All

--- application/controllers/Sso_reports.php
+++ application/controllers/Sso_reports.php
@@ -75,8 +75,8 @@ class Sso_reports extends CI_Controller {
         }
         $this->cli->command_help('All AD Users fetched',count($list));
         $this->cli->command_help("AD users with numeric id",$staff_with_numuric_id);
-        $this->cli->command_help("AD users with extension",$staff_with_numuric_id);
-        $this->cli->command_help("AD users with mobile",$staff_with_numuric_id);
+        $this->cli->command_help("AD users with extension",$staff_with_exten);
+        $this->cli->command_help("AD users with mobile",$staff_with_mobile);
 
     }
 
